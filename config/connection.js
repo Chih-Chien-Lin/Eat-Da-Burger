1976e613210b68bb9a621e13cf7b83f493526d46
Justin Lin <ro0566320@gmail.com>
2020-03-03 14:38:01 -0500

    update env

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-if (process.env.JAWS_DB_URL){
-  connection = mysql.createConnection(process.env.JAWS_DB_URL)
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
 var connection = mysql.createConnection({
   host: "localhost",
